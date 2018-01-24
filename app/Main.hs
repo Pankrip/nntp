98403b51c4c8f7a156055dd5b05c8e844c77f0e3
Pankrip <35636019+Pankrip@users.noreply.github.com>
2018-01-24 04:17:36 +0100

    testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
-import Data.ByteString.Lazy
-import Data.ByteString.Builder
-import System.Environment
+import qualified Data.ByteString.Lazy as L
+-- import Data.ByteString.Builder as B
+import qualified System.Environment as E
+import qualified Control.Concurrent as C
 --
 import Init
+import Options
+import Signals
 import MasterSocket
 
 main :: IO ()
@@ -17,7 +20,7 @@ main = (
 	\msock -> undefined
 	)
 -}
-main = undefined
+--main = undefined
 
 {-
  - main() components:
@@ -30,4 +33,25 @@ main = undefined
  - SyncStorage
  - exit
 -}
+
+-- proof of concept testing
+main = (
+	E.getArgs >>=
+	return . getConfig >>=
+	proceedOrFail
+	)
+
+proceedOrFail :: Either String Options -> IO ()
+proceedOrFail (Left str) = putStrLn str
+proceedOrFail (Right conf) = (
+	case (port conf) of
+		Nothing -> putStrLn "empty port"
+		Just p -> (
+			initMasterSocket (L.toStrict <$> (host conf)) (L.toStrict p) >>=
+			\sock -> (
+				C.myThreadId >>= installExitHandlers >>
+				acceptLoop sock 5
+				)
+			)
+	)
 
